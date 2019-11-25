4136aac59833b002803bc3c78ecd59c7f5e049bd
Bri Gab <brigabiola@outlook.com>
2019-11-24 20:05:48 -0700

    Functional

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mma'));
+// $("#currentDay").text(moment().format('MMMM Do YYYY, h:mma'));
+var userInput = $("#input1");
+var userInputTwo = $("#input2");
+var userInputThree = $("#input3");
+var userInputFour = $("#input4");
+var userInputFive = $("#input5");
+var userInputSix = $("#input6");
+var userInputSeven = $("#input7");
+var userInputEight = $("#input8");
+var userInputNine = $("#input9");
 
-    var userInput = $("#input");
-    var userInputTwo = $("#inputTwo");
-    var userInputThree = $("#inputThree");
-    var userInputFour = $("#inputFour");
-    var userInputFive = $("#inputFive");
-    var userInputSix = $("#inputSix");
-    var userInputSeven = $("#inputSeven");
-    var userInputEight = $("#inputEight");
-    var userInputNine = $("#inputNine");
-    var span1 = $("#span1");
 
+setInterval(function(){ 
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mma"));
+}, 1000);
+
+var schedule1 = localStorage.getItem("calendar1")
+var schedule2 = localStorage.getItem("calendar2")
+var schedule3 = localStorage.getItem("calendar3")
+var schedule4 = localStorage.getItem("calendar4")
+var schedule5 = localStorage.getItem("calendar5")
+var schedule6 = localStorage.getItem("calendar6")
+var schedule7 = localStorage.getItem("calendar7")
+var schedule8 = localStorage.getItem("calendar8")
+var schedule9 = localStorage.getItem("calendar9")
+  
 $(".save").on("click", function(event){
-    event.preventDefault();
+  event.preventDefault();
+
     localStorage.setItem("calendar1", userInput.val());
     localStorage.setItem("calendar2", userInputTwo.val());
     localStorage.setItem("calendar3", userInputThree.val());
@@ -21,9 +35,89 @@ $(".save").on("click", function(event){
     localStorage.setItem("calendar6", userInputSix.val());
     localStorage.setItem("calendar7", userInputSeven.val());
     localStorage.setItem("calendar8", userInputEight.val());
-    localStorage.setItem("calendar9", userInputNine.val());
-    var getItem = localStorage.getItem("calendar1");
-    span1.textContent = getItem;
-    
+    localStorage.setItem("calendar9", userInputEight.val());
+   
 })
+$("#input1").text(schedule1);
+$("#input2").text(schedule2);
+$("#input3").text(schedule3);
+$("#input4").text(schedule4);
+$("#input5").text(schedule5);
+$("#input6").text(schedule6);
+$("#input7").text(schedule7);
+$("#input8").text(schedule8);
+$("#input9").text(schedule9);
+
+
+if (moment().format("H") > 9) {
+  $("#input1").addClass("past");
+} else if (moment().format("H") < 9) {
+  $("#input1").addClass("future");
+} else if (moment().format("H") === 9) {
+  $("#input1").addClass("present")
+}
+
+if (moment().format("H") > 10) {
+  $("#input2").addClass("past");
+} else if (moment().format("H") < 10) {
+  $("#input2").addClass("future");
+} else if (moment().format("H") === 10) {
+  $("#input2").addClass("present")
+}
+
+if (moment().format("H") > 11) {
+  $("#input3").addClass("past");
+} else if (moment().format("H") < 11) {
+  $("#input3").addClass("future");
+} else if (moment().format("H") === 11) {
+  $("#input3").addClass("present")
+}
+
+if (moment().format("H") > 12) {
+  $("#input4").addClass("past");
+} else if (moment().format("H") < 12) {
+  $("#input4").addClass("future");
+} else if (moment().format("H") === 12) {
+  $("#input4").addClass("present")
+}
+
+if (moment().format("H") > 13) {
+  $("#input5").addClass("past");
+} else if (moment().format("H") < 13) {
+  $("#input5").addClass("future");
+} else if (moment().format("H") === 13) {
+  $("#input5").addClass("present")
+}
+
+if (moment().format("H") > 14) {
+  $("#input6").addClass("past");
+} else if (moment().format("H") < 14) {
+  $("#input6").addClass("future");
+} else if (moment().format("H") === 14) {
+  $("#input6").addClass("present")
+}
+
+if (moment().format("H") > 15) {
+  $("#input7").addClass("past");
+} else if (moment().format("H") < 15) {
+  $("#input7").addClass("future");
+} else if (moment().format("H") === 15) {
+  $("#input7").addClass("present")
+}
+if (moment().format("H") > 16) {
+  $("#input8").addClass("past");
+} else if (moment().format("H") < 16) {
+  $("#input8").addClass("future");
+} else if (moment().format("H") === 16) {
+  $("#input8").addClass("present")
+}
+if (moment().format("H") > 17) {
+  $("#input9").addClass("past");
+} else if (moment().format("H") < 17) {
+  $("#input9").addClass("future");
+} else if (moment().format("H") === 17) {
+  $("#input9").addClass("present")
+}
+//hint
+
 
